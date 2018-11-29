fee91b87a50f26b12d4a03e70f39da92e305371d
Pierre Faure--Giovagnoli <pierre.faure.giovagnoli@gmail.com>
2018-11-29 09:01:18 +0100

    structure improvments

--- agile-front/htdocs/js/Controller/states/delState.js
+++ agile-front/htdocs/js/Controller/states/delState.js
@@ -1,5 +1,7 @@
-class DelState{
+class DelState extends State{
     constructor(){
+        super();
+
         enableButtons(["#undo", "#redo", "#loadDel", "#loadRounds", "#loadMap", "#rmvDel", "#mapSelector", "#delSelector", "#addDel", "#addDel"]);
         disableButtons([""]);
         console.log("Etat delState"); 
@@ -10,58 +12,28 @@ class DelState{
     }
     
     handleScroll(evt){
-        let delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
-        let rate = delta/7;
-        Ctrl.View.zoom(rate);
-        
-        return evt.preventDefault() && false;
+        super.scroll(event);
     };
     
     
     handleMouseDown(evt){
-        let View = Ctrl.View;
-        
-        let ratio = View.Canvas.ratio;
-    
-        document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
-        View.lastX = ratio*evt.offsetX;
-        View.lastY = ratio*evt.offsetY;
-        Ctrl.View.clicked = true;
+        super.MouseDown(evt);
     }
     
     handleMouseMove(evt){
-        let View = Ctrl.View;
-        let ratio = View.Canvas.ratio;
-    
-        if(View.clicked){
-            View.dragged=true;
-        }else{
-            View.dragged=false;
-        }
-    
-        
-        if (View.dragged){
-            let newX = ratio*evt.offsetX;
-            let newY = ratio*evt.offsetY;
-            View.deltaX += newX-View.lastX;
-            View.deltaY += newY-View.lastY;
-            View.update();
-            View.lastX = newX;
-            View.lastY = newY;
-        }
-
+        super.MouseMove(evt);
     }
     
     handleMouseUp(evt){
         let View = Ctrl.View;
-        View.clicked=false;
-        if(View.dragged){
-            View.dragged = false;
-        }else if(evt.srcElement.tagName==="CANVAS"){
+        if(!Ctrl.dragged && evt.srcElement.tagName==="CANVAS"){
+            console.log("ok");
             let ratio = View.Canvas.ratio;
             var node = View.Map.findBestNode(ratio*(evt.offsetX-View.Canvas.html.offsetTop), ratio*(evt.offsetY-View.Canvas.html.offsetLeft));
             View.Deliveries.nodeInfos(node);
             View.update();
         }
+        
+        super.MouseUp(evt);
     }
 }
--- agile-front/htdocs/js/Controller/states/mapState.js
+++ agile-front/htdocs/js/Controller/states/mapState.js
@@ -1,5 +1,6 @@
-class MapState{
+class MapState extends State{
     constructor(){
+        super();
         enableButtons(["#undo", "#redo", "#loadDel", "#loadRounds", "#loadMap", "#mapSelector", "#delSelector", "#addDel"]);
         disableButtons(["#addDel", "#rmvDel"]);
         $("#snoInfoBox").hide();
@@ -7,74 +8,19 @@ class MapState{
     }
     
     handleScroll(evt){
-        console.log("d");
-        let delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
-        let rate = delta/7;
-        Ctrl.View.zoom(rate);
-        
-        return evt.preventDefault() && false;
+        super.scroll(evt);
     };
     
     
     handleMouseDown(evt){
-        console.log("e");
-        let View = Ctrl.View;
-        
-        let ratio = View.Canvas.ratio;
-    
-        document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
-        View.lastX = ratio*evt.offsetX;
-        View.lastY = ratio*evt.offsetY;
-        Ctrl.View.clicked = true;
+        super.MouseDown(evt);
     }
     
     handleMouseMove(evt){
-        let View = Ctrl.View;
-        let ratio = View.Canvas.ratio;
-    
-        if(View.clicked){
-            View.dragged=true;
-        }else{
-            View.dragged=false;
-        }
-    
-        
-        if (View.dragged){
-            let newX = ratio*evt.offsetX;
-            let newY = ratio*evt.offsetY;
-            View.deltaX += newX-View.lastX;
-            View.deltaY += newY-View.lastY;
-            View.update();
-            View.lastX = newX;
-            View.lastY = newY;
-        }
-        
-    
-        if(Ctrl.addingPoint || Ctrl.removePoint){
-            let nodeId = View.Map.findBestNode(ratio*(evt.offsetX-View.Canvas.html.offsetTop), ratio*(evt.offsetY-View.Canvas.html.offsetLeft));
-            View.Map.highlightNode(nodeId, View.Canvas.ctx);
-        }
+        super.MouseMove(evt);
     }
     
     handleMouseUp(evt){
-        //console.log(this.tagName);
-        let View = Ctrl.View;
-        View.clicked=false;
-        if(View.dragged){
-            View.dragged = false;
-        }else{
-            if(Ctrl.addingPoint && this.tagName==="CANVAS"){
-                let ratio = View.Canvas.ratio;
-                var node = View.Map.findBestNode(ratio*(evt.offsetX-View.Canvas.html.offsetTop), ratio*(evt.offsetY-View.Canvas.html.offsetLeft));
-                View.Deliveries.addUserNode(View.Map.coord[node]);
-                View.update();
-            } 
-            if(Ctrl.removePoint && this.tagName==="CANVAS"){
-                let ratio = View.Canvas.ratio;
-                var node = View.Map.findBestNode(ratio*(evt.offsetX-View.Canvas.html.offsetTop), ratio*(evt.offsetY-View.Canvas.html.offsetLeft));
-                View.Deliveries.removeNode(View.Map.coord[node]);
-                View.update();
-            } 
-        }
+        super.MouseUp(evt);
     }
 }
