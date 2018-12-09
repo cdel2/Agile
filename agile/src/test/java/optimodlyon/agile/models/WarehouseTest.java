5b39c7f7ad53b6297cc5caea9eeb17f92c88c519
cdel2 <cdelfossecsi@gmail.com>
2018-12-09 18:09:18 +0100

    modified sonar properties + TESTS

--- agile/src/test/java/optimodlyon/agile/models/WarehouseTest.java
+++ agile/src/test/java/optimodlyon/agile/models/WarehouseTest.java
@@ -4,26 +4,52 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class WarehouseTest {
+import optimodlyon.agile.util.Time;
 
+public class WarehouseTest {
 	@Test
 	public void testWarehouseLongFloatFloatTime() {
-		fail("Not yet implemented");
+		long id=1;
+		float latitude = 1;
+		float longitude = 1;
+		Time t = new Time(1,1,1);		
+		
+		Warehouse myWHS = new Warehouse(id, latitude, longitude, t);
+		assertNotNull(myWHS);
+		assertSame(myWHS.getId(),id);
+		assertSame(myWHS.getTimeStart(),t);
+		assertSame(myWHS.getLatitude(),latitude);
+		assertSame(myWHS.getLongitude(),longitude);
 	}
 
 	@Test
 	public void testWarehouseLongTime() {
-		fail("Not yet implemented");
+		long id=1;
+		Time t = new Time(1,1,1);		
+		
+		Warehouse myWHS = new Warehouse(id, t);
+		assertNotNull(myWHS);
+		assertSame(myWHS.getId(),id);
+		assertSame(myWHS.getTimeStart(),t);
 	}
 
 	@Test
 	public void testGetTimeStart() {
-		fail("Not yet implemented");
+		long id=1;
+		float latitude=-50;
+		float longitude=50;
+		Intersection myInter = new Intersection(id,latitude,longitude);
+		assertEquals(myInter.getLatitude(),-50,0);
 	}
 
 	@Test
 	public void testSetTimeStart() {
-		fail("Not yet implemented");
+		Time t = new Time(1,1,1);		
+		Time t2 = new Time(2,2,2);		
+		Warehouse myWHS = new Warehouse((long)1, (float)-50, (float)50, new Time(1,1,1));
+		assertEquals(myWHS.getTimeStart(),t);
+		myWHS.setTimeStart(new Time(2,2,2));
+		assertEquals(myWHS.getTimeStart(),t2);
 	}
 
 }
